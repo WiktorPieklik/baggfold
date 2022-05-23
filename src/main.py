9d5dc88608d1d90be0a1f7e79f98ea6c8d015369
WiktorPieklik <wiktor.pieklik@icloud.com>
2022-05-23 14:35:47 +0200

    refactor: sample code

--- src/main.py
+++ src/main.py
@@ -7,6 +7,8 @@ if __name__ == "__main__":
     lung_cancer, diabetes, credit_cards = datasets_paths()
     X, y = preprocess(diabetes)
     evaluate_reference_models(X, y)
+
+    # BaggFold SMA Oversampling
     steps = [('model', BaggFold())]
     description = ('BaggFold', 'SMA Oversampling')
     evaluate_model(steps=steps, X=X, y=y, model_description=description)
