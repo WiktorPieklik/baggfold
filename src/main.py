a8fdfb3ee49b34f2cd287ec2e2535f4b47852637
WiktorPieklik <wiktor.pieklik@icloud.com>
2022-05-30 08:28:24 +0200

    refactor: main script

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from evaluate import datasets_paths, evaluate_reference_models, evaluate_model
 from preprocessing import preprocess
 from baggfold import BaggFold
+from sklearn.svm import SVC
 
 
 if __name__ == "__main__":
@@ -9,7 +10,7 @@ if __name__ == "__main__":
     evaluate_reference_models(X, y)
 
     # BaggFold SMA Oversampling
-    steps = [('model', BaggFold())]
+    steps = [('model', BaggFold(lambda: SVC()))]
     description = ('BaggFold', 'SMA Oversampling')
     evaluate_model(steps=steps, X=X, y=y, model_description=description)
 
