fix!: removed standard scaling

src/preprocessing.py
@@ -3,7 +3,7 @@ import pandas as pd
 from typing import Optional, Tuple
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 
 
 def preprocess(dataset_path: str, categorical_data_column: Optional[int] = None) -> Tuple[np.array, np.array]:
@@ -16,17 +16,11 @@ def preprocess(dataset_path: str, categorical_data_column: Optional[int] = None)
     ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [categorical_data_column])],
                            remainder='passthrough')
     le = LabelEncoder()
-    sc = StandardScaler()
     if categorical_data_column is not None:
         X[:, categorical_data_column + 1:-1] = imputer.fit_transform(X=X[:, categorical_data_column + 1:-1])
         X = np.array(ct.fit_transform(X))
     else:
         X[:, :-1] = imputer.fit_transform(X=X[:, :-1])
     y = le.fit_transform(y=y)
-
-    if categorical_data_column is not None:
-        X[:, 2:] = sc.fit_transform(X=X[:, 2:])
-    else:
-        X = sc.fit_transform(X=X)
 
     return X, y