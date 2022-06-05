4277ba10a24cca5c258f21dabedb80e93bfd3ad0
WiktorPieklik <wiktor.pieklik@icloud.com>
2022-06-05 17:15:10 +0200

    feat!: stable testing

--- src/testing.py
+++ src/testing.py
@@ -6,7 +6,7 @@ from imblearn.under_sampling import RandomUnderSampler
 from imblearn.pipeline import Pipeline
 from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.base import clone
-from c45 import C45
+from sklearn.tree import DecisionTreeClassifier
 from xgboost import XGBClassifier
 from sklearn.svm import SVC
 from preprocessing import preprocess
@@ -21,28 +21,6 @@ from alive_progress import alive_bar
 from baggfold import ThreadedBaggFold
 
 import numpy as np
-
-# lung_cancer, diabetes, credit_cards
-# datasets = datasets_paths()[:2]
-classifiers_types = {
-    # 'C45': C45(),
-    'SVM': SVC(),
-    'XGB': XGBClassifier(),
-}
-techniques = {
-    'NONE': None,
-    'SMOTE': SMOTE(),
-    'RND_OVER': RandomOverSampler(),
-    'RND_UNDER': RandomUnderSampler(),
-}
-pipelines = []
-models_description = []
-for classifier in classifiers_types.items():
-    for technique in techniques.items():
-        steps = [(technique[0], technique[1]), (classifier[0], classifier[1])]
-        models_description.append(f"{classifier[0]} {technique[0]}")
-        pipelines.append(Pipeline(steps=steps))
-pipelines.append(Pipeline(steps=[('NONE', None), ('BaggFold', ThreadedBaggFold(lambda: SVC()))]))
 
 
 class StatisticalTest:
@@ -103,8 +81,6 @@ class StatisticalTest:
             SUCCESS(f"Result saved in {save_file}")
 
     def __analyze_statistics(self) -> None:
-        # t_statistics = np.zeros((len(self.__pipelines), len(self.__pipelines)))
-        # p_value = np.zeros((len(self.__pipelines), len(self.__pipelines)))
         WARNING("\n\n[2/2] Analyzing statistics...")
         for dataset in self.__scores.keys():
             self.__t_statistics[dataset] = np.zeros((len(self.__pipelines), len(self.__pipelines)))
@@ -172,14 +148,12 @@ class StatisticalTest:
     def __get_dataset_name(self, dataset_path: str) -> str:
         path = Path(dataset_path)
         suffix = path.suffix
-        dataset_name = path.name.removesuffix(suffix)
+        dataset_name = path.name[:-4]
 
         return dataset_name
 
     def __show_config(self) -> None:
         self.__started_at = datetime.now().strftime(self.__dt_format)
-        # files = [self.__construct_file_name(dataset_path) for dataset_path in self.__datasets]
-        # files_info = f"Results will be saved in: {', '.join(files)}"
         pipelines_info = [["Classifier"], ["Preprocessing technique(s)"]]
         for pipeline in self.__pipelines:
             classifier, preprocessings = self.__pipeline_credentials(pipeline)
@@ -189,7 +163,6 @@ class StatisticalTest:
 
         ##########
         INFO(self.__test_type)
-        # INFO(files_info)
         WARNING(f"Provided classificators and techniques:\n{pipelines_info}")
         IMPORTANT("Caution! These tests might take a while! Be patient")
 
@@ -199,86 +172,26 @@ class StatisticalTest:
         self.__analyze_statistics()
 
 
-# def conduct_test(n_splits: int, n_repeats: int, file_name: str):
-#     repeated_stratified_k_fold = RepeatedStratifiedKFold(n_splits=n_splits, n_repeats=n_repeats, random_state=1)
-#
-#     scores = np.zeros((len(pipelines), len(datasets), n_splits * n_repeats))
-#
-#     for dataset_id, dataset in enumerate(datasets):
-#         measurement_start = time()
-#         print(f"{Fore.YELLOW} Testing: {dataset}")
-#         dataset_name = Path(dataset).name
-#         contains_categorical_col = True if dataset_id == 0 else False
-#         X, y = preprocess(dataset, 0 if contains_categorical_col else None)
-#         for fold_id, (train, test) in enumerate(repeated_stratified_k_fold.split(X, y)):
-#             X_train, X_test = X[train], X[test]
-#             y_train, y_test = y[train], y[test]
-#             X_train, sc = standard_scaler(X_train, contains_categorical_col)
-#             if contains_categorical_col:
-#                 X_test = sc.transform(X=X_test[:, 2:])
-#             else:
-#                 X_test = sc.transform(X=X_test)
-#
-#             for pipeline_id, pipeline in enumerate(pipelines):
-#                 pipeline = clone(pipeline)
-#                 pipeline.fit(X_train, y_train)
-#                 scores[pipeline_id, dataset_id, fold_id] = scorer(pipeline, X_test, y_test)
-#         measurement_stop = time()
-#         execution_time = measurement_stop - measurement_start
-#         print(f"{Fore.GREEN} {dataset_name} took: {execution_time} seconds")
-#
-#     np.save(file_name, scores)
-#     print(f"{Fore.RESET}")  # reset colors
-#
-#
-# def analyze_results(result_file_name: str) -> None:
-#     scores = np.load(f"{result_file_name}.npy")
-#     print(f"Scores: {scores.shape}")
-#
-#
-# def perform_statistics(scores: np.array, alpha: float) -> str:
-#     t_statistics = np.zeros((len(pipelines), len(pipelines)))
-#     p_value = np.zeros((len(pipelines), len(pipelines)))
-#     for i in range(len(pipelines)):
-#         for j in range(len(pipelines)):
-#             for k in range(len(datasets)):
-#                 t_statistics[i, j], p_value[i, j] = ttest_rel(scores[i][k], scores[j][k])
-#
-#     t_statistics = np.nan_to_num(t_statistics)
-#     p_value = np.nan_to_num(p_value, nan=1.0)
-#
-#     headers = models_description
-#     column_names = np.array(list(map(lambda name: [name], headers)))
-#     t_statistic_table = np.concatenate((column_names, t_statistics), axis=1)
-#     t_statistic_table = tabulate(t_statistic_table, headers, floatfmt=".2f")
-#     p_value_table = np.concatenate((column_names, p_value), axis=1)
-#     p_value_table = tabulate(p_value_table, headers, floatfmt=".2f")
-#     print(f"t-statistic:\n{t_statistic_table}\n\np-value:\n{p_value_table}\n\n")
-#
-#     advantage = np.zeros((len(pipelines), len(pipelines)))
-#     advantage[t_statistics > 0] = 1
-#     advantage_table = tabulate(np.concatenate(
-#         (column_names, advantage), axis=1)
-#         , headers)
-#     print(f"Advantage:\n{advantage_table}\n\n")
-#
-#     significance = np.zeros((len(pipelines), len(pipelines)))
-#     significance[p_value <= alpha] = 1
-#     significance_table = tabulate(np.concatenate(
-#         (column_names, significance), axis=1
-#     ), headers)
-#     print(f"Statistical significance (alpha = {alpha}):\n{significance_table}\n\n")
-#
-#     statistically_better = significance * advantage
-#     statistically_better_table = tabulate(np.concatenate(
-#         (column_names, statistically_better), axis=1
-#     ), headers)
-#     print(f"Statistically significantly better:\n{statistically_better_table}")
-#
-#
 if __name__ == "__main__":
-    # conduct_test(2, 5, "results")
-    # analyze_results("results")
-    # pipeline = [Pipeline(steps=[('Threaded BaggFold', None), ('SMA Oversampling', None), ('TBaggFold', ThreadedBaggFold(lambda: SVC()))])]
+    classifiers_types = {
+        # 'C45': C45(), # very poor implementation. Way to slow to conduct tests!!!
+        'CART': DecisionTreeClassifier(),
+        'SVM': SVC(),
+        'XGB': XGBClassifier(),
+    }
+    techniques = {
+        'NONE': None,
+        'SMOTE': SMOTE(),
+        'RND_OVER': RandomOverSampler(),
+        'RND_UNDER': RandomUnderSampler(),
+    }
+    pipelines = []
+    # reference methods
+    for classifier in classifiers_types.items():
+        for technique in techniques.items():
+            steps = [(technique[0], technique[1]), (classifier[0], classifier[1])]
+            pipelines.append(Pipeline(steps=steps))
+    # BaggFold SMA Oversampling
+    pipelines.append(Pipeline(steps=[('NONE', None), ('BaggFold', ThreadedBaggFold(lambda: SVC()))]))
     test = StatisticalTest(datasets_paths(), pipelines, .05, 2, 5)
     test.run()
